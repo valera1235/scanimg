4fbf2e2eabc68268be117cffaa833117e0225015
deltabest <deltabest@ro.ru>
2025-04-02 00:41:44 +0300

    Add Started

--- __main__.py
+++ __main__.py
@@ -20,3 +20,4 @@ if __name__ == "__main__": # выполняем при запуске именн
     #logging.basicConfig(level=logging.INFO) # запуск логгирования
     #asyncio.run(main()) # асинхронный запуск главной процедуры
     print("Hi!")
+    print("We started this project!")
