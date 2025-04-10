37fbe1f6c9a2c65faf56c2016fcac1dd5cd9cd7a
plishkinvicktor <plishkinvicktor@yandex.ru>
2025-04-10 22:36:46 +0300

    Update doctr.py

--- doctr.py
+++ doctr.py
@@ -1,1 +1,4 @@
 from doctr.models import ocr_predictor
+model = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_vgg16_bn')
+result = model(['document.jpg'])
+print(result.export()['words'])  # Список распознанных слов/чисел
